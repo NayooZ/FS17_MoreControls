411d332fcf1016e4be4234bc9d62551cd45517dd
NayooZ <37068647+NayooZ@users.noreply.github.com>
2018-03-05 21:19:38 +1100

    Add files via upload

---
+++ MoreControls.lua
@@ -0,0 +1,40 @@
+-- 
+-- MoreControls
+-- 
+-- @Interface: 1.5.3.1 b1848
+-- @Author: Nayoo 
+-- @Date: 05.03.2018
+-- @Version: 1.0.0
+-- 
+-- @Support: https://www.reddit.com/message/compose/?to=Nayoo
+-- 
+
+MoreControls = {}
+
+function MoreControls.prerequisitesPresent(specializations)
+    return SpecializationUtil.hasSpecialization(Cylindered, specializations);
+end;
+
+function MoreControls:load(savegame)
+end;
+
+function MoreControls:delete()
+end;
+
+function MoreControls:mouseEvent(posX, posY, isDown, isUp, button)
+end;
+
+function MoreControls:keyEvent(unicode, sym, modifier, isDown)
+end;
+
+function MoreControls:update(dt)
+end;
+      
+function MoreControls:draw()
+	if self:getIsActiveForInput(true) then
+		g_currentMission:addExtraPrintText("|Y|                                                                         Extra Control A");
+		g_currentMission:addExtraPrintText("|Z|                                                                         Extra Control B");
+		g_currentMission:addExtraPrintText("|N|                                                                         Extra Control C");
+--You can add/remove lines as necessary
+	end;
+end;
